6b9eaa29d6fa0f82ac3872fec5305a271fe012e5
Anastasiia_Buhaienko <anastasiia.buhaienko@gmail.com>
2023-02-27 21:29:09 +0200

    first point

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,43 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+
+// 1.Создание и рендер разметки по массиву данных galleryItems и предоставленному шаблону элемента галереи.
+
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarkup = createGalleryImage(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup)
+
+function createGalleryImage(galleryItems) {
+
+    return galleryItems.map(({ preview, original, description }) => {
+        return `
+        <div class="gallery__item">
+            <a class="gallery__link">
+                <img
+                    src="${preview}"
+                    alt="${description}"
+                    class="gallery__image"
+                >
+            </a>
+        </div>
+        `;
+    })
+        .join('');
+
+}
+
+
+
+
+
+
+// 2.Реализация делегирования на div.gallery и получение url большого изображения.
+// 3.Подключение скрипта и стилей библиотеки модального окна basicLightbox.Используй CDN сервис jsdelivr 
+// и добавь в проект ссылки на минифицированные(.min) файлы библиотеки.
+// 4.Открытие модального окна по клику на элементе галереи. Для этого ознакомься с документацией и примерами.
+// 5.Замена значения атрибута src элемента < img > в модальном окне перед открытием.Используй готовую разметку 
+// модального окна с изображением из примеров библиотеки basicLightbox.
