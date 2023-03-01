d3070767992c7ef3858598141eb23edbf7a573af
Anastasiia_Buhaienko <anastasiia.buhaienko@gmail.com>
2023-03-01 16:20:07 +0200

    Task 2 and some update in task 1

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,34 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarkup = createGalleryCard(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup)
+
+function createGalleryCard(galleryItems) {
+    
+    return galleryItems.map(({ preview, original, description }) => {
+        return `
+        <a class="gallery__item" href="${original}">
+            <img 
+                class="gallery__image" 
+                src="${preview}" 
+                alt="${description}" 
+            />
+        </a>
+        `;
+    })
+        .join('');
+
+};
+
+const lightbox = new SimpleLightbox('.gallery a',
+    {
+    captionsData: 'alt',
+    captionPosition:'outside',
+    captionDilay: '250',    
+});
